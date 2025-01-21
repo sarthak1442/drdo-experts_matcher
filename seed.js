5ec911fc50b2dba5d98f1d40e244563e06e62f47
Sarthak Singh Rathore <131984558+sarthak1442@users.noreply.github.com>
2025-01-21 13:44:11 +0530

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const JobPost = require("./models/JobPost");
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017/drdo_recruitment", {
+mongoose.connect(process.env.MONGODB_URI_DRDO, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
